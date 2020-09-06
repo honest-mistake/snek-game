70dd7650340375752188e1421d03f02e6d853191
honest-mistake <32182897+honest-mistake@users.noreply.github.com>
2020-09-06 09:59:21 -0700

    added initial canvas grid rendering

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,27 @@
 // including SCSS
 import '../css/main.scss';
 
-const foo = () => {
-    console.log('hello world');
-};
+const canvasNode = document.getElementById('game-viewport');
+const ctx = canvasNode.getContext('2d');
+const gridBoxSize = 40;
+const height = 600; // TODO: get this dynamically later
+const width = 800; // TODO: get this dynamically later
+const rowCount = Math.floor(height / gridBoxSize);
+const colCount = Math.floor(width / gridBoxSize);
 
-foo();
-foo();
+ctx.strokeStyle = '#ccc';
+let count = 0;
+
+for (let xIndex = 0; xIndex < colCount; xIndex += 1) {
+    for (let yIndex = 0; yIndex < rowCount; yIndex += 1) {
+        count++;
+        ctx.strokeRect(
+            xIndex * gridBoxSize,
+            yIndex * gridBoxSize,
+            gridBoxSize,
+            gridBoxSize
+        );
+    }
+}
+
+console.log(count);
