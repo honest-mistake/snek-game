aad19c2b3849e26f3a41338d04fb025532c8cacf
honest-mistake <32182897+honest-mistake@users.noreply.github.com>
2020-09-07 11:43:11 -0700

    added initial game loop, added reusable draw functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,103 @@
 // including SCSS
 import '../css/main.scss';
 
-const canvasNode = document.getElementById('game-viewport');
-const ctx = canvasNode.getContext('2d');
+const timeBetweenTicks = 250;
 const gridBoxSize = 40;
 const height = 600; // TODO: get this dynamically later
 const width = 800; // TODO: get this dynamically later
-const rowCount = Math.floor(height / gridBoxSize);
-const colCount = Math.floor(width / gridBoxSize);
+let canvasNode;
+let ctx;
+let rowCount;
+let colCount;
+let snakeX;
+let snakeY;
 
-ctx.strokeStyle = '#ccc';
-let count = 0;
+const setCommonVars = () => {
+    canvasNode = document.getElementById('game-viewport');
+    ctx = canvasNode.getContext('2d');
+    rowCount = Math.floor(height / gridBoxSize);
+    colCount = Math.floor(width / gridBoxSize);
+    snakeX = 10;
+    snakeY = 10;
+};
 
-for (let xIndex = 0; xIndex < colCount; xIndex += 1) {
-    for (let yIndex = 0; yIndex < rowCount; yIndex += 1) {
-        count++;
-        ctx.strokeRect(
-            xIndex * gridBoxSize,
-            yIndex * gridBoxSize,
-            gridBoxSize,
-            gridBoxSize
-        );
+const drawDebugGrid = () => {
+    const color = '#ccc';
+
+    for (let xIndex = 0; xIndex < colCount; xIndex += 1) {
+        for (let yIndex = 0; yIndex < rowCount; yIndex += 1) {
+            drawBoxOutline(color, xIndex, yIndex);
+        }
     }
-}
+};
 
-console.log(count);
+const drawSnek = () => {
+    drawBox('green', snakeX, snakeY);
+};
+
+const drawTarget = () => {
+    drawBox('red', 3, 3);
+};
+
+const drawSomething = (colorType, drawType) => (color, xPos, yPos) => {
+    ctx[colorType] = color;
+    ctx[drawType](
+        xPos * gridBoxSize,
+        yPos * gridBoxSize,
+        gridBoxSize,
+        gridBoxSize
+    );
+};
+
+const drawBoxOutline = drawSomething('strokeStyle', 'strokeRect');
+
+const drawBox = drawSomething('fillStyle', 'fillRect');
+
+const clearCanvas = () => {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(
+        0,
+        0,
+        width,
+        height
+    );
+};
+
+const startGameLoop = () => {
+    tick();
+};
+
+const renderAllTheStuff = () => {
+    clearCanvas();
+    drawDebugGrid();
+    drawTarget();
+    drawSnek();
+};
+
+// TODO: update snake position
+// TODO: update snake length?
+// TODO: move snake forward but also trim tail?
+const updateSnake = () => {
+    if (snakeX < colCount - 1) {
+        snakeX += 1;
+    } else {
+        snakeX = 0;
+    }
+};
+
+const tick = () => {
+    setTimeout(() => {
+        updateSnake();
+        renderAllTheStuff();
+        tick();
+    }, timeBetweenTicks);
+};
+
+(function initialize () {
+    // set game mode select
+    // set common variables
+    // do first time game setup
+    setCommonVars();
+    renderAllTheStuff();
+    startGameLoop();
+})();
